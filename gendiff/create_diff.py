21dc1efb9c06aff8c19387297a7838e0541ff420
Dima <dima_rood@inbox.ru>
2023-11-15 21:53:13 +0300

    editing a module3

--- gendiff/create_diff.py
+++ gendiff/create_diff.py
@@ -14,22 +14,25 @@ def get_diff_key(data1, data2):
             dict_['old_value'] = data1_value
 
         elif key in data1 and key in data2:
+
             if data1_value == data2_value:
                 dict_['status'] = 'unchanged'
                 dict_["old_value"] = data1_value
-        
-        elif isinstance(data1_value, dict) and isinstance(data2_value, dict):
-            dict_['status'] = 'nested'
-            dict_['nested'] = data1_value, data2_value
-        
+
+            elif isinstance(data1_value, dict) and isinstance(data2_value, dict):
+                dict_['status'] = 'nested'
+                dict_['nested'] = data1_value, data2_value
+            else:
+                dict_['status'] = 'updated'
+                dict_['old_value'] = data1_value
+                dict_['new_value'] = data2_value
+
         elif key not in data1 and key in data2:
             dict_['status'] = 'added'
             dict_['new_value'] = data2_value
-        
-        else:
-            dict_['status'] = 'updated'
-            dict_['old_value'] = data1_value
-            dict_['new_value'] = data2_value
-        diff.append(dict_)
+
+        child= dict([('key', key)])
+        child.update(dict_)
+        diff.append(child)
 
     return diff
